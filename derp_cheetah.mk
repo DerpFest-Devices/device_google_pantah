a2493ea67e823c8ebb10645937823e79b6f4d2b2
NurKeinNeid <mralexman3000@gmail.com>
2022-11-04 21:03:43 +0000

    pantah: Derp it
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- derp_cheetah.mk
+++ derp_cheetah.mk
@@ -4,20 +4,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common DerpFest stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+TARGET_IS_PIXEL := true
+TARGET_IS_PIXEL_7 := true
+TARGET_PIXEL_STAND_SUPPORTED := true
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_cheetah.mk)
-$(call inherit-product, device/google/gs201/lineage_common.mk)
+$(call inherit-product, device/google/gs201/derp_common.mk)
 
-include device/google/pantah/cheetah/device-lineage.mk
+include device/google/pantah/cheetah/device-derp.mk
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 7 Pro
-PRODUCT_NAME := lineage_cheetah
+PRODUCT_NAME := derp_cheetah
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 3120
@@ -28,5 +32,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="cheetah-user 14 UQ1A.240205.002 11224170 release-keys"
 
 BUILD_FINGERPRINT := google/cheetah/cheetah:14/UQ1A.240205.002/11224170:user/release-keys
+
+DERP_BUILDTYPE := Official
 
 $(call inherit-product, vendor/google/cheetah/cheetah-vendor.mk)
--- derp_panther.mk
+++ derp_panther.mk
@@ -4,20 +4,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common DerpFest stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+TARGET_IS_PIXEL := true
+TARGET_IS_PIXEL_7 := true
+TARGET_PIXEL_STAND_SUPPORTED := true
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_panther.mk)
-$(call inherit-product, device/google/gs201/lineage_common.mk)
+$(call inherit-product, device/google/gs201/derp_common.mk)
 
-include device/google/pantah/panther/device-lineage.mk
+include device/google/pantah/panther/device-derp.mk
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 7
-PRODUCT_NAME := lineage_panther
+PRODUCT_NAME := derp_panther
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
@@ -28,5 +32,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="panther-user 14 UQ1A.240205.002 11224170 release-keys"
 
 BUILD_FINGERPRINT := google/panther/panther:14/UQ1A.240205.002/11224170:user/release-keys
+
+DERP_BUILDTYPE := Official
 
 $(call inherit-product, vendor/google/panther/panther-vendor.mk)
